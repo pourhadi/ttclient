cf98d9d68723071b2833f788edc186654727033a
Daniel Pourhadi <dan@pourhadi.com>
2021-02-18 04:44:46 -0600

    Initial Commit

---
+++ Shared/ttClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ttClientApp.swift
+//  Shared
+//
+//  Created by Daniel Pourhadi on 2/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ttClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
